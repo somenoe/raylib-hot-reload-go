41e05cf80b4d3c118b0700fc09f089a77ca201c0
somenoe <otashi24@gmail.com>
2024-12-19 22:11:03 +0700

    refactor: Remove unused draw function

--- main.go
+++ main.go
@@ -9,15 +9,6 @@ const (
 	targerFPS    = 60
 )
 
-func draw() {
-	r.BeginDrawing()
-
-	r.ClearBackground(r.Black)
-	r.DrawText("Congrats! You created your first window!", 190, 200, 20, r.Green)
-
-	r.EndDrawing()
-}
-
 func main() {
 	r.InitWindow(screenWidth, screenHeight, title)
 	defer r.CloseWindow()
